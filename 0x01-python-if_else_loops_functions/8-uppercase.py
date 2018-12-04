8af3fb22e5d45dc8c4898ba230f5b5e6b852669a
AlexYu01 <tengalexyu@gmail.com>
2018-12-04 20:33:00 +0000

    Reduce print usage

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,10 @@
 
 def uppercase(str):
     for c in str[:]:
-        c_ascii = ord(c)
-        if c_ascii >= 97 and c_ascii <= 122:
-            print('{}'.format(chr(c_ascii - 32)), end="")
+        val = ord(c)
+        if val >= 97 and val <= 122:
+            w = chr(val - 32)
         else:
-            print('{}'.format(c), end="")
+            w = c
+        print('{:s}'.format(w), end="")
     print()
