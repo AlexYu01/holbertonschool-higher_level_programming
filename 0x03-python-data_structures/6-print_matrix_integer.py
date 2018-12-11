23df500a1b33a68cb2e922a0f636a66158360321
AlexYu01 <tengalexyu@gmail.com>
2018-12-11 20:42:09 +0000

    Use length of each row

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in row[:-1]:
-            print("{:d}".format(i), end=' ')
-        for last in row[-1:]:
-            print("{:d}".format(last))
+        length = len(row)
+        for i in range(length):
+            if i < length - 1:
+                print("{:d}".format(row[i]), end=' ')
+            else:
+                print("{:d}".format(row[i]))
