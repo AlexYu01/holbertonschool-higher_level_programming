781823389e64c40b734165e5c1fd8717ec51e50f
AlexYu01 <tengalexyu@gmail.com>
2018-12-11 17:26:58 +0000

    Commit task 6: Prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row[:-1]:
+            print("{:d}".format(i), end=' ')
+        for last in row[-1:]:
+            print("{:d}".format(last))
