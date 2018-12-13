c185c1fb884761245fc7a833d32684c1ba693358
AlexYu01 <tengalexyu@gmail.com>
2018-12-13 22:35:20 +0000

    Remove old directory

--- 0x04-python-more_data_structure/3-common_elements.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def common_elements(set_1, set_2):
-    if isinstance(set_1, set) and isinstance(set_2, set):
-        return set_1 & set_2
--- 0x04-python-more_data_structure/6-print_sorted_dictionary.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    if isinstance(a_dictionary, dict):
-        for key in sorted(a_dictionary):
-            print("{0}: {1}".format(key, a_dictionary[key]))
