07f4efe895ea54d272e0e12283afa05063947a0a
AlexYu01 <tengalexyu@gmail.com>
2019-01-02 20:31:28 +0000

    Catch any exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as err:
+    except Exception as err:
         sys.stderr.write("Exception: " + err.__str__() + "\n")
         return False
     else:
