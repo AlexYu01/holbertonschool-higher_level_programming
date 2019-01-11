be09b987b88f12652aff5ab4df582f7ede70e528
AlexYu01 <tengalexyu@gmail.com>
2019-01-11 03:24:54 +0000

    Commit task 31: Prevent instance attributes being added unless they are

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Module for `LockedClass`
+"""
+
+
+class LockedClass:
+    """
+        LockedClass that wont allow any instance attributes to be added except
+        for `first_name`.
+    """
+
+    def __setattr__(self, name, value):
+        """
+            Override the reserved method `setattr` to prevent instance
+            attributes besides `first_name` to be add to the instance's dict.
+
+            Args:
+                name (:obj:`str`): The name of the proposed attribute.
+                value (any): value to assign to proposed attribute.
+        """
+        if name != "first_name":
+            raise AttributeError("'" + self.__class__.__name__ +
+                                 "' object has no attribute '" +
+                                 name + "'")
+        self.__dict__[name] = value
