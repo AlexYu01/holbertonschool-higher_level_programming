2f2716e0aa2b9dd2c0a74939326d0d1f06d8cd83
AlexYu01 <tengalexyu@gmail.com>
2019-01-16 19:59:16 +0000

    Commit task 6: Implement an area function

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/bin/usr/python3
+"""
+    Module containing the `BaseGeometry` class.
+"""
+
+
+class BaseGeometry:
+    """A class.
+    """
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
