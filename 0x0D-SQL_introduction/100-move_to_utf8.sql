860aff13b2d1f3adc7ee917ba70d037cc938bc5e
AlexYu01 <tengalexyu@gmail.com>
2019-03-04 19:49:05 +0000

    Commit task 17: Convert a database and table to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- Change character set and collate to UTF8
+-- Change the database: `hbtn_0c_0`i to UTF8
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Change all and future columns in `first_table` to UTF8
+ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
