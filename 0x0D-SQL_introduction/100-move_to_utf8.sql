d40fa4aff237d31c8e190686af67db4d8ec1a1d3
AlexYu01 <tengalexyu@gmail.com>
2019-03-04 21:55:11 +0000

    Convert table and specific column separately

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,4 +2,6 @@
 -- Change the database: `hbtn_0c_0`i to UTF8
 ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Change all and future columns in `first_table` to UTF8
-ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `first_table` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Change specific column to utf8
+ALTER TABLE `first_table` MODIFY `name` VARCHAR(256) CHARACTER SET utf8;
