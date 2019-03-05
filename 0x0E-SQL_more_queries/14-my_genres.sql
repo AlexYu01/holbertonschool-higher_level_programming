f898730cb77a0273c1769cc60d41fc3b0f522c21
AlexYu01 <tengalexyu@gmail.com>
2019-03-05 15:45:22 -0800

    Order by genre name

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -5,4 +5,5 @@ FROM tv_shows s INNER JOIN tv_show_genres sg
   ON s.id = sg.show_id
 INNER JOIN tv_genres g
   ON sg.genre_id = g.id
-WHERE s.title = 'Dexter';
+WHERE s.title = 'Dexter'
+ORDER BY g.name;
