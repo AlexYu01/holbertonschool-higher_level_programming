407b282d111068f54ea0b186be09e5cd4f347367
AlexYu01 <tengalexyu@gmail.com>
2019-03-30 01:47:12 +0000

    add sensitive check

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 
     cur = conn.cursor()
-    stmt = "SELECT id, name FROM states WHERE name = '{}' ORDER BY id ASC"\
-        .format(sys.argv[4])
+    stmt = "SELECT id, name FROM states WHERE BINARY name = '{}' ORDER BY id \
+    ASC".format(sys.argv[4])
     cur.execute(stmt)
     query_rows = cur.fetchall()
 
