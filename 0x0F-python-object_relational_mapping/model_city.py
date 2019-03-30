cdab3647e3565d2cf90e068a809130a1f2661159
AlexYu01 <tengalexyu@gmail.com>
2019-03-30 01:08:40 +0000

    Commit task 14: Get a list of all cities, their id, and the state they reside in

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, text, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+"""
+    Module that performs creates a States class based off of Base.
+"""
+
+
+class City(Base):
+    """
+        The ``City`` class which inherits from ``Base`` class.
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
+
+    state = relationship("State", back_populates="cities")
+
+State.cities = relationship("City", order_by=City.id, back_populates="state")
