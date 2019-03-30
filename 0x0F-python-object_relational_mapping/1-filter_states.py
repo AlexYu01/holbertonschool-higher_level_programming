93faf9afee266a64aded08993e25f1034d2b9ce7
AlexYu01 <tengalexyu@gmail.com>
2019-03-30 01:29:27 +0000

    Explicitely state column names

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 
     cur = conn.cursor()
-    stmt = "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    stmt = "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY \
+    states.id ASC"
     cur.execute(stmt)
     query_rows = cur.fetchall()
 
