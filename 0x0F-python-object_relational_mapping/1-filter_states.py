86c689ad736246e180f07d94f1e60b6e8efce0ec
AlexYu01 <tengalexyu@gmail.com>
2019-03-30 01:30:29 +0000

    Dont use utf8

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+                           passwd=sys.argv[2], db=sys.argv[3])
 
     cur = conn.cursor()
     stmt = "SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY \
