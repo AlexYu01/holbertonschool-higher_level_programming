d42fa12c65e2f9c5aa9305775e4a2f49f92d19ca
AlexYu01 <tengalexyu@gmail.com>
2019-03-30 02:10:39 +0000

    Print Nothing

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -24,6 +24,6 @@ if __name__ == "__main__":
     if state is not None:
         print("{}: {}".format(state.id, state.name))
     else:
-        print('')
+        print('Nothing')
 
     session.close()
