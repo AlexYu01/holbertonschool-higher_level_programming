a548344b8504b61017b6c3c9dd6dae7f54250680
AlexYu01 <tengalexyu@gmail.com>
2019-04-11 02:24:55 +0000

    Commit task 1: Display body if the http status is 200

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Curl a URL and display body if the status is 200
+RESP=$(curl -sLI "$1" -X GET)
+STATUS=$(echo "$RESP" | grep "200 OK" | cut -d' '  -f2)
+if [ "$STATUS" = 200 ];
+then
+    curl -sL "$1"
+fi
