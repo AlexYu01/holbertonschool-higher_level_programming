9161cdb421dceefc47851ae110e5c1d4372d099b
AlexYu01 <tengalexyu@gmail.com>
2019-04-11 02:36:56 +0000

    Compare 200 as a string

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -2,7 +2,7 @@
 # Curl a URL and display body if the status is 200
 RESP=$(curl -sLI "$1" -X GET)
 STATUS=$(echo "$RESP" | grep "200 OK" | cut -d' '  -f2)
-if [ "$STATUS" = 200 ];
+if [ "$STATUS" = '200' ];
 then
     curl -sL "$1"
 fi
