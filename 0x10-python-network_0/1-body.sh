e6a2b59348cd6347e14e8374fb1028e857ac4358
AlexYu01 <tengalexyu@gmail.com>
2019-04-11 02:40:26 +0000

    Crunch into 3 lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Curl a URL and display body if the status is 200
-RESP=$(curl -sLI "$1" -X GET)
-STATUS=$(echo "$RESP" | grep "200 OK" | cut -d' '  -f2)
-if [ "$STATUS" = '200' ];
-then
-    curl -sL "$1"
-fi
+if [ "$(curl -sLI "$1" -X GET | grep "200 OK" | cut -d' ' -f2)" = '200' ]; then curl -sL "$1"; fi
