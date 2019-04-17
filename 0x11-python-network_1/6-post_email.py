e4aeb43cd52bec281cf9704a087f9e096533bc63
AlexYu01 <tengalexyu@gmail.com>
2019-04-17 22:43:38 +0000

    Fix pep8

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    resp = requests.post(sys.argv[1], data = {'email': sys.argv[2]})
+    resp = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     if resp is not None:
         print(resp.text)
