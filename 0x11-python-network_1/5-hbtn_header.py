fe21ab2af3f3fb2845be6c6530077636f906e1e6
AlexYu01 <tengalexyu@gmail.com>
2019-04-17 22:44:56 +0000

    Take parameters

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,8 +3,9 @@
     Module for getting the X-Request-Id of intranet.hbtn.io/status.
 """
 import requests
+import sys
 
 if __name__ == "__main__":
-    resp = requests.get('https://intranet.hbtn.io/status')
+    resp = requests.get(sys.argv[1])
     if resp is not None:
         print(resp.headers.get('X-Request-Id'))
