ca44a0ca9a7226748cf2fc8dba3ae2be1731d7d5
AlexYu01 <tengalexyu@gmail.com>
2019-05-14 16:47:30 +0000

    Commit task 4: Count the number of appearances of a value in the body response of an API query

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  } else if (resp.statusCode === 200 && resp.headers['content-type'] === 'application/json') {
+    let results = JSON.parse(body).results;
+    let count = 0;
+    let wedgeAntilles = 'https://swapi.co/api/people/18/';
+    for (let i = 0; i < results.length; i++) {
+      let chars = results[i].characters;
+      for (let j = 0; j < chars.length; j++) {
+        if (chars[j] === wedgeAntilles) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
